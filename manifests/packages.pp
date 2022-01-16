bfc9fe5514bbfbee5b52fbde8945424a40db67c1
Sebastian Rakel <sebastian@devunit.eu>
2022-01-16 22:21:18 +0100

    .

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -2,9 +2,11 @@ class minecraft::packages {
   if $minecraft::manage_java {
     class { 'java':
       distribution => 'jre',
-      version      => 'latest',
+      version      => '17',
     }
   }
 
-  ensure_packages('screen')
+  if $minecraft::service_provider == 'rc' {
+    ensure_packages('screen')
+  }
 }
